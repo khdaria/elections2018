b53bf7139c8e45b9a9367cba306999accb455b27
khdaria <kharkina.daria@gmail.com>
2018-03-20 12:34:00 +0100

    data gathering

---
+++ data_scrapping.R
@@ -0,0 +1,62 @@
+library(tidyverse)
+library(rvest)
+library(stringr)
+
+
+url <- "http://www.vybory.izbirkom.ru/region/region/izbirkom?action=show&root=1&tvd=100100084849066&vrn=100100084849062&region=0&global=1&sub_region=0&prver=0&pronetvd=null&vibid=100100084849066&type=226"
+page <- read_html(url)
+url_regions <- page %>% html_nodes(xpath="//select[@name='gs']//option") %>% html_attr("value")
+url_regions <- url_regions[-1]
+
+eng_names <- c("voters_in_list", 
+               "ballots_received",
+               "ballots_issued_early",
+               "ballots_issued_polling_st",
+               "ballots_issued_outside_polling_st",
+               "canceled_ballots",
+               "ballots_mobile_boxes",
+               "ballots_stationary_boxes",
+               "invalid_ballots",
+               "valid_ballots",
+               "lost_ballots",
+               "not_recorded_ballots",
+               "Baburin", "Grudinin", "Zhirinovsky", "Putin", "Sobchak", "Suraykin", "Titov", "Yavlinsky")
+
+
+elections <- data.frame()
+
+for(i in url_regions){
+  print(which(url_regions==i))
+  page <- read_html(i)
+  Sys.sleep(.5)
+  url_regions2 <- page %>% html_nodes(xpath="//form[@name='go_reg']//option") %>% html_attr("value")
+  url_regions2 <- url_regions2[-1]
+  url_regions2 <- str_replace_all(url_regions2, "region=0&sub_region=0", "region=1&sub_region=1")
+  print(str_c(length(url_regions2), " regions"))
+  
+  for(j in url_regions2){
+    page <- read_html(j)
+    Sys.sleep(.5)
+    url_regions3 <- page %>% html_nodes(xpath="//select[@name='gs']//option") %>% html_attr("value")
+    url_regions3 <- url_regions3[-1]
+    print(str_c(length(url_regions3), " subregions"))
+    for(l in url_regions3){
+      page <- read_html(l, encoding="windows-1251")
+      Sys.sleep(.5)
+      tab <- page %>% html_node(xpath="//table[6]") %>% html_table()
+      tab <- filter(tab, !is.na(X1)) %>% select(-1)
+      tab$X2 <- eng_names
+      tab <- tab %>% separate(X3,into = c("value","prop"), sep="\\r\\n",fill="right", convert = T) %>%
+        select(-prop) %>% spread(X2,value) %>% select(eng_names)
+      reg <- page %>% html_nodes(xpath="//table[3]//tr[1]//a") %>% html_text()
+      tab$l1 <- reg[1]
+      tab$l2 <- reg[2]
+      tab$l3 <- reg[3]
+      
+      elections <- rbind(elections, tab)
+      
+    }
+  }
+}
+
+write.csv(elections, "elections.csv")
